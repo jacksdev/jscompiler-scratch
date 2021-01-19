12b4e789171465b3f4eef0736490028cb78fdcb3
abc <jacksdevwork@gmail.com>
2021-01-18 16:22:22 -0800

    tokenizer changed from switch to if conditions

--- tokenizer.js
+++ tokenizer.js
@@ -1,41 +1,30 @@
-
 const LETTERS = /[a-z]/i;
-
-module.exports = function tokenizer(input){
-
-    const tokens = [];
-    
-    let current = 0;
-    while (current < input.length) {
-        let char = input[current];
-
-        switch(char) {
-            case '(':
-                tokens.push({
-                    type: 'paren',
-                    value: '('
-                  });
-                  current++;
-                  continue;
-            break;
-            case LETTERS.test(char):
-                let value = '';
-                while (LETTERS.test(char)) {
-                    value += char;
-                    char = input[++current];
-                }
-                tokens.push({
-                    type: 'name',
-                    value
-                  });
-                  continue;
-            break;
-          }
-
-        throw new TypeError(`Unknown char: '${char}'`)
+module.exports = function tokenizer(input) {
+  const tokens = [];
+  let current = 0;
+  while (current < input.length) {
+    let char = input[current];
+    if (char === '(') {
+      tokens.push({
+        type: 'paren',
+        value: '('
+      });
+      current++;
+      continue;
     }
-
-
-    return tokens;
-    
+    if (LETTERS.test(char)) {
+      let value = '';
+      while (LETTERS.test(char)) {
+        value += char;
+        char = input[++current];
+      }
+      tokens.push({
+        type: 'name',
+        value
+      });
+      continue;
+    }
+    throw new TypeError(`Unknown char: '${char}'`);
+  }
+  return tokens;
 }
