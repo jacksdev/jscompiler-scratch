2c8e67e1d27344f860770610a032248b8ac24bff
abc <jacksdevwork@gmail.com>
2021-01-22 17:59:04 -0800

    set up parser...need to fix extra parentaces bug

--- compiler.js
+++ compiler.js
@@ -9,14 +9,18 @@ module.exports = function compiler(input){
   //      Breaks the input code (string) into the basic syntax
   //      of the language (array of objects)
   ////////////////////////////////////////////////////////////
-
   const tokens = tokenizer(input);
 
 
   // 2. Syntactic Analysis
+  //      Transforms the tokens (array of objects) into an
+  //      AST (tree of objects) which represents our program
+  const lispAST = parser(tokens);
+
+
   // 3. Transformation
   // 4. Code Generation
   //
-  return tokens;
+  return lispAST;
   
 }       
